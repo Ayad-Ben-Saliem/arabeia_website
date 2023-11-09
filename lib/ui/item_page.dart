314de0638fbf44c5fd102f2f81e6274ca9a70fdd
Ayad <ayad.bslm@gmail.com>
2023-11-09 15:25:14 +0200

    - Fix a bug in item_page.dart   1- ConstrainedBox.maxHeight should set to constraints.maxHeight instead of double.infinity   2- Remove IntrinsicHeight

--- lib/ui/item_page.dart
+++ lib/ui/item_page.dart
@@ -19,26 +19,30 @@ class ItemPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(),
-      body: Builder(builder: (context) {
-        if (item != null) return ItemView(item: item!);
-        return Center(
-          child: FutureBuilder<Item?>(
-            future: Database.getItem(id!),
-            builder: (ctx, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                if (snapshot.hasData) {
-                  return ItemView(item: snapshot.requireData!);
+      body: Builder(
+        builder: (context) {
+          if (item != null) return ItemView(item: item!);
+          return Center(
+            child: FutureBuilder<Item?>(
+              future: Database.getItem(id!),
+              builder: (ctx, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  if (snapshot.hasData) {
+                    return ItemView(item: snapshot.requireData!);
+                  }
+                } else if (snapshot.connectionState ==
+                    ConnectionState.waiting) {
+                  return const CircularProgressIndicator();
                 }
-              } else if (snapshot.connectionState == ConnectionState.waiting) {
-                return const CircularProgressIndicator();
-              }
-              return Text('Error!! ${snapshot.error}');
-            },
-          ),
-        );
-      }),
+                return Text('Error!! ${snapshot.error}');
+              },
+            ),
+          );
+        },
+      ),
     );
   }
 }
@@ -52,90 +56,84 @@ class ItemView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(builder: (context, constraints) {
-      return SingleChildScrollView(
-        child: Center(
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return SingleChildScrollView(
           child: ConstrainedBox(
-            constraints: constraints.copyWith(
-              minHeight: constraints.maxHeight,
-              maxHeight: double.infinity,
+            constraints: BoxConstraints(
+              maxHeight: constraints.maxHeight,
               maxWidth: 720,
             ),
-            child: IntrinsicHeight(
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  if (item.images.isNotEmpty)
-                    ImageCarousel(images: item.images),
-                  Align(
-                    alignment: Alignment.center,
-                    child: Padding(
-                      padding: const EdgeInsets.all(24),
-                      child: Text(
-                        item.name,
-                        style: const TextStyle(fontSize: 18),
-                      ),
-                    ),
-                  ),
-                  if (item.description?.isNotEmpty == true)
-                    Padding(
-                      padding: const EdgeInsets.all(24),
-                      child: Text(
-                        item.description!,
-                        style: const TextStyle(fontSize: 12),
-                      ),
-                    ),
-                  Expanded(child: Container()),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 8),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (item.images.isNotEmpty) ImageCarousel(images: item.images),
+                Align(
+                  alignment: Alignment.center,
+                  child: Padding(
+                    padding: const EdgeInsets.all(24),
                     child: Text(
-                      'الحجم',
-                      style: TextStyle(fontWeight: FontWeight.bold),
+                      item.name,
+                      style: const TextStyle(fontSize: 18),
                     ),
                   ),
+                ),
+                if (item.description?.isNotEmpty == true)
                   Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8),
-                    child: Wrap(
-                      spacing: 4.0,
-                      children: [
-                        for (final size in item.sizes)
-                          Consumer(
-                            builder: (context, ref, widget) {
-                              final selected = size == ref.watch(sizeProvider);
-                              return ActionChip(
-                                label: Text(
-                                  size,
-                                  style: TextStyle(
-                                    color: selected
-                                        ? Theme.of(context)
-                                            .colorScheme
-                                            .onPrimary
-                                        : null,
-                                    fontWeight: FontWeight.w400,
-                                  ),
-                                ),
-                                backgroundColor: selected
-                                    ? Theme.of(context).colorScheme.primary
-                                    : null,
-                                onPressed: () {
-                                  ref.read(sizeProvider.notifier).state = size;
-                                },
-                              );
-                            },
-                          ),
-                      ],
+                    padding: const EdgeInsets.all(24),
+                    child: Text(
+                      item.description!,
+                      style: const TextStyle(fontSize: 12),
                     ),
                   ),
-                  const Divider(),
-                  footer(),
-                ],
-              ),
+                const Spacer(),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8),
+                  child: Text(
+                    'الحجم',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  child: Wrap(
+                    spacing: 4.0,
+                    children: [
+                      for (final size in item.sizes)
+                        Consumer(
+                          builder: (context, ref, widget) {
+                            final selected = size == ref.watch(sizeProvider);
+                            return ActionChip(
+                              label: Text(
+                                size,
+                                style: TextStyle(
+                                  color: selected
+                                      ? Theme.of(context).colorScheme.onPrimary
+                                      : null,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                              backgroundColor: selected
+                                  ? Theme.of(context).colorScheme.primary
+                                  : null,
+                              onPressed: () {
+                                ref.read(sizeProvider.notifier).state = size;
+                              },
+                            );
+                          },
+                        ),
+                    ],
+                  ),
+                ),
+                const Divider(),
+                footer(),
+              ],
             ),
           ),
-        ),
-      );
-    });
+        );
+      },
+    );
   }
 
   Widget footer() {
@@ -151,13 +149,13 @@ class ItemView extends StatelessWidget {
               return ElevatedButton(
                 onPressed: (ref.watch(sizeProvider) != null)
                     ? () =>
-                        ref.read(CartNotifier.itemsProvider.notifier).addItem(
-                              CartItem(
-                                item: item,
-                                size: ref.read(sizeProvider)!,
-                                quantity: 1,
-                              ),
-                            )
+                    ref.read(CartNotifier.itemsProvider.notifier).addItem(
+                      CartItem(
+                        item: item,
+                        size: ref.read(sizeProvider)!,
+                        quantity: 1,
+                      ),
+                    )
                     : null,
                 child: const Text('إضافة للسلة'),
               );
