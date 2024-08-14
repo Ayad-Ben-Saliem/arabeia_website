a2ac893899196a09f449739976438def48fd0184
Abdullah <abdms646@gmail.com>
2024-08-14 13:02:55 +0200

    Disable Material3

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -17,13 +17,13 @@ class App extends ConsumerWidget {
   Widget build(BuildContext context, ref) {
     return MaterialApp(
       title: 'عربية',
-
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ref.watch(darkMode)
             ? const ColorScheme.dark(primary: Colors.white)
             : const ColorScheme.light(primary: Colors.black),
         fontFamily: 'HacenTunisia',
+        useMaterial3: false,
       ),
       locale: const Locale('ar'),
       initialRoute: '/',
