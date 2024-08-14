d4995a088f06656563581a5cf8c3d2514ad33eba
Abdullah <abdms646@gmail.com>
2024-08-14 13:10:45 +0200

    Use shared preferences to save user information.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
-import 'package:arabiya/db/db.dart';
 import 'package:arabiya/ui/app.dart';
 import 'package:flutter/material.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'firebase_options.dart';
+
+late final SharedPreferences sharedPreferences;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  SharedPreferences.getInstance().then((preferences) => sharedPreferences = preferences);
 
   runApp(const ProviderScope(child: App()));
 }
