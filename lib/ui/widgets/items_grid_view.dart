2100f3f730cb62346ae441e90508f8618c4cd670
Abdullah <abdms646@gmail.com>
2024-09-11 12:43:58 +0200

    improve

--- lib/ui/widgets/items_grid_view.dart
+++ lib/ui/widgets/items_grid_view.dart
@@ -11,6 +11,8 @@ class ItemsGridView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
+      // TODO: Implement infinite pagination scroll here
+
       builder: (context, constraints) {
         int crossAxisCount(BoxConstraints constraints) {
           return (constraints.maxWidth / 500).ceil();
@@ -19,34 +21,31 @@ class ItemsGridView extends StatelessWidget {
         double aspectRatio(BoxConstraints constraints) {
           final width = constraints.maxWidth / crossAxisCount(constraints);
 
+          /*
+           image carousel:
+           carousel indicators: 36
+           name: 64
+           description: 128
+           sizes: 64
+           footer: 64
+           */
           // image carousel + carousel indicators + name and footer
           // final height = width / (16 / 9) + 36 + 128 + 118;
           final height = width / (16 / 9) + 36 + 128 + 128;
           return width / height;
         }
 
-        final List<Widget> cards = [
-          if (editable)
-            Card(
-              child: InkWell(
-                onTap: () => Navigator.pushNamed(context, '/add-item'),
-                child: const FittedBox(
-                  fit: BoxFit.contain,
-                  child: Icon(Icons.add),
-                ),
-              ),
-            ),
-          ...items.map((item) => ItemCard(item: item, editable: editable)),
-        ];
-
-        return GridView(
+        return GridView.builder(
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: crossAxisCount(constraints),
             childAspectRatio: aspectRatio(constraints),
           ),
           shrinkWrap: true,
           physics: const ClampingScrollPhysics(),
-          children: cards,
+          itemCount: items.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ItemCard(item: items.elementAt(index), editable: editable);
+          },
         );
       },
     );
