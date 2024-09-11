1149beb36b513698bea8845dfb36fa98ec047ac3
Abdullah <abdms646@gmail.com>
2024-09-11 12:48:56 +0200

    `ItemPage` and `ItemView` are separated.

--- lib/ui/item_page.dart
+++ lib/ui/item_page.dart
@@ -1,11 +1,9 @@
 import 'package:arabiya/db/db.dart';
-import 'package:arabiya/models/invoice_item.dart';
 import 'package:arabiya/models/item.dart';
-import 'package:arabiya/ui/cart_notifier.dart';
-import 'package:arabiya/ui/home_page.dart';
-import 'package:arabiya/ui/widgets/item_card.dart';
+import 'package:arabiya/ui/widgets/custom_indicator.dart';
+import 'package:arabiya/ui/widgets/item_view.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 
 class ItemPage extends StatelessWidget {
   final String? id;
@@ -21,186 +19,37 @@ class ItemPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Builder(
-        builder: (context) {
-          if (item != null) return ItemView(item: item!);
-          return Center(
-            child: FutureBuilder<Item?>(
-              future: Database.getItem(id!),
-              builder: (ctx, snapshot) {
-                if (snapshot.connectionState == ConnectionState.done) {
-                  if (snapshot.hasData) {
-                    return ItemView(item: snapshot.requireData!);
-                  }
-                } else if (snapshot.connectionState ==
-                    ConnectionState.waiting) {
-                  return const CircularProgressIndicator();
-                }
-                return Text('Error!! ${snapshot.error}');
-              },
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: Center(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(minHeight: constraints.maxHeight, maxWidth: 720),
+                child: Builder(
+                  builder: (context) {
+                    if (item != null) return ItemView(item: item!);
+                    return Center(
+                      child: FutureBuilder<Item?>(
+                        future: Database.getItem(id!),
+                        builder: (ctx, snapshot) {
+                          if (snapshot.connectionState == ConnectionState.done) {
+                            if (snapshot.hasData) {
+                              return ItemView(item: snapshot.requireData!);
+                            }
+                          } else if (snapshot.connectionState == ConnectionState.waiting) {
+                            return const CustomIndicator();
+                          }
+                          return Text('Error!! ${snapshot.error}');
+                        },
+                      ),
+                    );
+                  },
+                ),
+              ),
             ),
           );
         },
       ),
     );
-  }
-}
-
-class ItemView extends StatelessWidget {
-  final Item item;
-
-  ItemView({super.key, required this.item});
-
-  final sizeProvider = StateProvider<String?>((ref) => null);
-
-  @override
-  Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        return SingleChildScrollView(
-          child: Center(
-            child: ConstrainedBox(
-              constraints: BoxConstraints(
-                minHeight: constraints.maxHeight,
-                maxWidth: 720,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    children: [
-                      if (item.images.isNotEmpty)
-                        ImageCarousel(images: item.images),
-                      Padding(
-                        padding: const EdgeInsets.all(24),
-                        child: Text(
-                          item.name,
-                          style: const TextStyle(fontSize: 18),
-                        ),
-                      ),
-                    ],
-                  ),
-                  if (item.description?.isNotEmpty == true)
-                    Padding(
-                      padding: const EdgeInsets.all(24),
-                      child: Text(
-                        item.description!,
-                        style: const TextStyle(fontSize: 15),
-                      ),
-                    ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 8),
-                        child: Text(
-                          'الحجم',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8),
-                        child: Wrap(
-                          spacing: 4.0,
-                          runSpacing: 4.0,
-                          children: [
-                            for (final size in item.sizes)
-                              Consumer(
-                                builder: (context, ref, widget) {
-                                  final selected =
-                                      size == ref.watch(sizeProvider);
-                                  return ActionChip(
-                                    label: Text(
-                                      size,
-                                      style: TextStyle(
-                                        color: selected
-                                            ? Theme.of(context)
-                                                .colorScheme
-                                                .onPrimary
-                                            : null,
-                                        fontWeight: FontWeight.w400,
-                                      ),
-                                    ),
-                                    backgroundColor: selected
-                                        ? Theme.of(context).colorScheme.primary
-                                        : null,
-                                    onPressed: () {
-                                      ref.read(sizeProvider.notifier).state =
-                                          size;
-                                    },
-                                  );
-                                },
-                              ),
-                          ],
-                        ),
-                      ),
-                      const Divider(height: 32),
-                      footer(),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-        );
-      },
-    );
-  }
-
-  Widget footer() {
-    return Row(
-      children: [
-        const SizedBox(width: 8),
-        priceWidget(),
-        const Spacer(),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Consumer(
-            builder: (context, ref, child) {
-              return ElevatedButton(
-                onPressed: (ref.watch(sizeProvider) != null)
-                    ? () =>
-                        ref.read(CartNotifier.itemsProvider.notifier).addItem(
-                              InvoiceItem(
-                                item: item,
-                                size: ref.read(sizeProvider)!,
-                                quantity: 1,
-                              ),
-                            )
-                    : null,
-                child: const Text('إضافة للسلة'),
-              );
-            },
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget priceWidget() {
-    if (item.discount != null) {
-      return Column(
-        children: [
-          Text(
-            '${item.price} $currency',
-            style: const TextStyle(
-              fontSize: 18,
-              color: Colors.grey,
-              decoration: TextDecoration.lineThrough,
-            ),
-          ),
-          Text(
-            '${item.discountedPrice} $currency',
-            style: const TextStyle(fontSize: 18),
-          ),
-        ],
-      );
-    } else {
-      return Text(
-        '${item.price} $currency',
-        style: const TextStyle(fontSize: 18),
-      );
-    }
   }
 }
