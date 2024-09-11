8e775819341e9524a7a6d235c35c6bc5dadc7c53
Abdullah <abdms646@gmail.com>
2024-09-11 12:25:15 +0200

    Improved the interface display to be adaptive according to the type of device and replaced `Google Maps` with `FlutterMap`

--- lib/ui/user_address_page.dart
+++ lib/ui/user_address_page.dart
@@ -1,19 +1,19 @@
-import 'dart:async';
-
-import 'package:arabiya/main.dart';
+import 'package:arabiya/ui/widgets/custom_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_map/flutter_map.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:arabiya/main.dart';
 
 final nameProvider = StateProvider((ref) => sharedPreferences.getString('name') ?? '');
 final phoneProvider = StateProvider((ref) => sharedPreferences.getString('phone') ?? '');
 final addressProvider = StateProvider((ref) => sharedPreferences.getString('address') ?? '');
 final locationProvider = StateProvider<LatLng>(
   (ref) => LatLng(
-    sharedPreferences.getDouble('latitude') ?? 32.8829352,
-    sharedPreferences.getDouble('longitude') ?? 13.1677362,
+    sharedPreferences.getDouble('latitude') ?? 32.904396192173024,
+    sharedPreferences.getDouble('longitude') ?? 13.262683314191843,
   ),
 );
 
@@ -21,92 +21,247 @@ class UserAddressPage extends ConsumerWidget {
   const UserAddressPage({super.key});
 
   @override
-  Widget build(context, ref) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: const Text('بيانات المستلم'),
       ),
-      body: Center(
-        child: ConstrainedBox(
-          constraints: const BoxConstraints(maxWidth: 1024),
+      body: width > 600 ? pcBody(context, ref) : mobileBody(context, ref),
+    );
+  }
+
+  Widget pcBody(BuildContext context, WidgetRef ref) {
+    return Center(
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextFormField(
-                  controller: TextEditingController(
-                    text: ref.read(nameProvider),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // address details
+                  Expanded(
+                    flex: 1,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: TextFormField(
+                            // controller: TextEditingController(
+                            //   text: ref.read(nameProvider),
+                            // ),
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'اسم المستلم',
+                            ),
+                            onChanged: (txt) {
+                              ref.read(nameProvider.notifier).state = txt;
+                            },
+                            textInputAction: TextInputAction.next,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: TextFormField(
+                            controller: TextEditingController(
+                              text: ref.read(phoneProvider),
+                            ),
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'رقم هاتف المستلم',
+                            ),
+                            onChanged: (txt) {
+                              ref.read(phoneProvider.notifier).state = txt;
+                            },
+                            keyboardType: TextInputType.phone,
+                            autocorrect: false,
+                            inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                            textInputAction: TextInputAction.next,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: TextFormField(
+                            controller: TextEditingController(
+                              text: ref.read(addressProvider),
+                            ),
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'عنوان الاستلام',
+                              hintText: 'المدينة، المنطقة، الشارع، أقرب نقطة دالة',
+                            ),
+                            onChanged: (txt) {
+                              ref.read(addressProvider.notifier).state = txt;
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'اسم المستلم',
+                  const SizedBox(width: 16.0),
+                  // map
+                  Expanded(
+                    flex: 1,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 8.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10.0),
+                        child: AspectRatio(
+                          aspectRatio: 4 / 3,
+                          child: FlutterMap(
+                            options: MapOptions(
+                              initialCenter: ref.watch(locationProvider),
+                              initialZoom: 15.0,
+                              onTap: (tapPosition, point) {
+                                ref.watch(locationProvider.notifier).state = point;
+                              },
+                            ),
+                            children: [
+                              TileLayer(
+                                urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                                subdomains: const ['a', 'b', 'c'],
+                              ),
+                              MarkerLayer(
+                                markers: [
+                                  Marker(
+                                    width: 80.0,
+                                    height: 80.0,
+                                    point: ref.watch(locationProvider),
+                                    child: const Icon(
+                                      Icons.location_on,
+                                      color: Colors.red,
+                                      size: 40.0,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
-                  onChanged: (txt) {
-                    ref.read(nameProvider.notifier).state = txt;
-                  },
-                  textInputAction: TextInputAction.next,
-                ),
+                ],
               ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextFormField(
-                  controller: TextEditingController(
-                    text: ref.read(phoneProvider),
-                  ),
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'رقم هاتف المستلم',
-                  ),
-                  onChanged: (txt) {
-                    ref.read(phoneProvider.notifier).state = txt;
-                  },
-                  keyboardType: TextInputType.phone,
-                  autocorrect: false,
-                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                  textInputAction: TextInputAction.next,
-                ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: _isValid(ref) ? confirm(context, ref) : null,
+                child: const Text('تأكيد'),
               ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextFormField(
-                  controller: TextEditingController(
-                    text: ref.read(addressProvider),
-                  ),
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'عنوان الاستلام',
-                    hintText: 'المدينة، المنطقة، الشارع، أقرب نقطة دالة',
-                  ),
-                  onChanged: (txt) {
-                    ref.read(addressProvider.notifier).state = txt;
-                  },
-                ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: LocationMap(
-                    target: ref.read(locationProvider),
-                    onMapMove: (position) {
-                      ref.read(locationProvider.notifier).state = position.target;
-                    },
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Consumer(
-                  builder: (context, ref, widget) {
-                    return ElevatedButton(
-                      onPressed: _isValid(ref) ? confirm(context, ref) : null,
-                      child: const Text('تأكيد'),
-                    );
-                  },
-                ),
-              )
             ],
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget mobileBody(BuildContext context, WidgetRef ref) {
+    return Center(
+      child: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextFormField(
+                // controller: TextEditingController(
+                //   text: ref.read(nameProvider),
+                // ),
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'اسم المستلم',
+                ),
+                onChanged: (txt) {
+                  ref.read(nameProvider.notifier).state = txt;
+                },
+                textInputAction: TextInputAction.next,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextFormField(
+                // controller: TextEditingController(
+                //   text: ref.read(phoneProvider),
+                // ),
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'رقم هاتف المستلم',
+                ),
+                onChanged: (txt) {
+                  ref.read(phoneProvider.notifier).state = txt;
+                },
+                keyboardType: TextInputType.phone,
+                autocorrect: false,
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                textInputAction: TextInputAction.next,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextFormField(
+                // controller: TextEditingController(
+                //   text: ref.read(addressProvider),
+                // ),
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'عنوان الاستلام',
+                  hintText: 'المدينة، المنطقة، الشارع، أقرب نقطة دالة',
+                ),
+                onChanged: (txt) {
+                  ref.read(addressProvider.notifier).state = txt;
+                },
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10.0),
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child: FlutterMap(
+                    options: MapOptions(
+                      initialCenter: ref.watch(locationProvider),
+                      initialZoom: 15.0,
+                      onLongPress: (tapPosition, point) {
+                        ref.watch(locationProvider.notifier).state = point;
+                      },
+                    ),
+                    children: [
+                      TileLayer(
+                        urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                        subdomains: const ['a', 'b', 'c'],
+                      ),
+                      MarkerLayer(
+                        markers: [
+                          Marker(
+                            width: 80.0,
+                            height: 80.0,
+                            point: ref.watch(locationProvider),
+                            child: const Icon(
+                              Icons.location_on,
+                              color: Colors.red,
+                              size: 40.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: _isValid(ref) ? confirm(context, ref) : null,
+              child: const Text('تأكيد'),
+            ),
+          ],
         ),
       ),
     );
@@ -120,70 +275,9 @@ class UserAddressPage extends ConsumerWidget {
     sharedPreferences.setString('name', ref.read(nameProvider));
     sharedPreferences.setString('phone', ref.read(phoneProvider));
     sharedPreferences.setString('address', ref.read(addressProvider));
-    sharedPreferences.setDouble('latitude', ref.read(locationProvider).latitude);
-    sharedPreferences.setDouble('longitude', ref.read(locationProvider).longitude);
+    sharedPreferences.setDouble('latitude', ref.watch(locationProvider).latitude);
+    sharedPreferences.setDouble('longitude', ref.watch(locationProvider).longitude);
 
     return () => Navigator.pushNamed(context, '/checkout');
-  }
-}
-
-class LocationMap extends StatelessWidget {
-  final LatLng target;
-
-  final Completer<GoogleMapController> _completer = Completer<GoogleMapController>();
-
-  final CameraPositionCallback? onMapMove;
-
-  LocationMap({super.key, required this.target, this.onMapMove});
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high
-        ),
-        builder: (context, snapshot) {
-          // if (snapshot.hasError) {
-//   return Center(child: Text('${snapshot.error}');
-
-          // }
-
-        if (snapshot.hasData) {
-          goto(
-            CameraPosition(
-              target: LatLng(
-                snapshot.data!.latitude,
-                snapshot.data!.longitude,
-              ),
-              zoom: 12,
-            ),
-          );
-        }
-
-        return Stack(
-          children: [
-            GoogleMap(
-              initialCameraPosition: CameraPosition(target: target, zoom: 14),
-              onMapCreated: (controller) => _completer.complete(controller),
-              myLocationEnabled: true,
-              onCameraMove: onMapMove,
-            ),
-            if (snapshot.connectionState == ConnectionState.waiting) const Center(child: CircularProgressIndicator()),
-            const Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [Icon(Icons.pin_drop_outlined), Text('مكان التوصيل')],
-              ),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  Future<GoogleMapController> get controller async => _completer.future;
-
-  Future<void> goto(CameraPosition position) async {
-    final controller = await _completer.future;
-    return controller.animateCamera(CameraUpdate.newCameraPosition(position));
   }
 }
