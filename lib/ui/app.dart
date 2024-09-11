0fdf3b2c627ed5cfcd79d68036e2e1a527bbc20c
Abdullah <abdms646@gmail.com>
2024-09-11 13:03:41 +0200

    Support light and dark mode and add new page links

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -1,18 +1,36 @@
+import 'package:arabiya/main.dart';
 import 'package:arabiya/models/item.dart';
 import 'package:arabiya/ui/add_edit_item_page.dart';
 import 'package:arabiya/ui/cart_page.dart';
 import 'package:arabiya/ui/checkout_page.dart';
 import 'package:arabiya/ui/home_page.dart';
-import 'package:arabiya/ui/management_page.dart';
+import 'package:arabiya/ui/invoice_page.dart';
+import 'package:arabiya/ui/appearance_page.dart';
+import 'package:arabiya/ui/invoices_page.dart';
+import 'package:arabiya/ui/items_management_page.dart';
+import 'package:arabiya/ui/reports_page.dart';
 import 'package:arabiya/ui/item_page.dart';
 import 'package:arabiya/ui/user_address_page.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide Router;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_router/flutter_router.dart' as FRouter;
+import 'package:flutter_router/flutter_router.dart';
 
-import 'invoice_page.dart';
+enum Mode {
+  dark,
+  light;
 
-final darkMode = StateProvider((ref) => false);
+  static Mode fromString(String str) {
+    for (final mode in values) {
+      if (str == '$mode') return mode;
+    }
+
+    throw UnsupportedError('Unsupported Mode ($str)');
+  }
+}
+
+final mode = StateProvider((ref) => Mode.fromString(sharedPreferences.getString('mode') ?? '${Mode.dark}'));
+
+const currency = 'د.ل';
 
 class App extends ConsumerWidget {
   const App({super.key});
@@ -20,33 +38,31 @@ class App extends ConsumerWidget {
   @override
   Widget build(BuildContext context, ref) {
     return MaterialApp(
-      title: 'عربية',
+      title: 'عَرَبِيَّة',
       debugShowCheckedModeBanner: false,
-
       theme: ThemeData(
-          colorScheme: ref.watch(darkMode) ? const ColorScheme.dark(primary: Colors.white) : const ColorScheme.light(primary: Colors.black),
-          fontFamily: 'HacenTunisia',
-          useMaterial3: false,
-          appBarTheme: const AppBarTheme(centerTitle: true)),
+        colorScheme: ref.watch(mode) == Mode.dark ? const ColorScheme.dark(primary: Colors.white) : const ColorScheme.light(primary: Colors.blue),
+        fontFamily: 'HacenTunisia',
+        useMaterial3: false,
+        appBarTheme: const AppBarTheme(centerTitle: true),
+      ),
       locale: const Locale('ar'),
       initialRoute: '/',
-
       builder: (context, child) {
         return Directionality(
           textDirection: TextDirection.rtl,
           child: child!,
         );
       },
-
-      onGenerateRoute: FRouter.Router({
+      onGenerateRoute: Router({
         '/cart': (ctx, match, settings) => const CartPage(),
         '/address': (ctx, match, settings) => const UserAddressPage(),
         '/checkout': (ctx, match, settings) => const CheckoutPage(),
         '/add-item': (ctx, match, settings) => const AddEditItemPage(),
         '/edit-item': (ctx, match, settings) => AddEditItemPage(
-          item: settings.arguments as Item?,
-          id: match!.parameters['id'],
-        ),
+              item: settings.arguments as Item?,
+              id: match!.parameters['id'],
+            ),
         '/invoices/{invoiceId}': (ctx, match, settings) {
           return InvoicePage(invoiceId: match!.parameters['invoiceId']!);
         },
@@ -54,13 +70,14 @@ class App extends ConsumerWidget {
           return ItemPage(
             item: settings.arguments as Item?,
             id: match!.parameters['id'],
-
           );
         },
-        '/management': (ctx, match, settings) => const ManagementPage(),
+        '/appearance': (ctx, match, settings) => const AppearancePage(),
+        '/invoices': (ctx, match, settings) => const InvoicesPage(),
+        '/items': (ctx, match, settings) => const ItemsManagementPage(),
+        '/reports': (ctx, match, settings) => const ReportsPage(),
         '/': (ctx, match, settings) => const HomePage(),
       }).get,
-
     );
   }
 }
