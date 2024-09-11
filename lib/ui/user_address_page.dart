76e313b50f21bceeb05b371474e4e36408668912
Abdullah <abdms646@gmail.com>
2024-09-11 12:38:05 +0200

    Remove unnecessary imports

--- lib/ui/user_address_page.dart
+++ lib/ui/user_address_page.dart
@@ -1,9 +1,7 @@
-import 'package:arabiya/ui/widgets/custom_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:arabiya/main.dart';
 
